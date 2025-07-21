d14325fc3fa15133ae77f07673e0cfd0009a4211
MadeehaM <madeehamissa@gmail.com>
2025-07-21 16:47:51 +0100

    new

--- conditionals/exameligibility.py
+++ conditionals/exameligibility.py
@@ -2,8 +2,11 @@ total = int(input("Enter total number of working days:"))
 print("Total number of working days is:", total)
 absent= int(input("Enter number of absent days:"))
 print ("total number of absent days is:", absent)
+present= total - absent
+percentage = (present/total)*100
+print(f"Attendance percentage:{percentage}")
 
-if (total- absent) /total * 100 < 75:
+if percentage < 75:
     print("You are not eligible to write this exam")
 else:
     print("You are eligible to write this exam")
