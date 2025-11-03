b1dde21db9c9072c250a3098607af4c6b3b5a26a
MadeehaM <madeehamissa@gmail.com>
2025-11-03 19:48:22 +0100

    class

---
+++ bitplay/5.py
@@ -0,0 +1,21 @@
+def TwoOdd(arr,size):
+    xorof2 = arr[0]
+    x=0
+    y=0
+    SetBit = 0
+    for i in range(1,size):
+        xorof2 = xorof2 ^ arr[i]
+    SetBit = xorof2 & ~(xorof2-1)
+    for i in range(size):
+        if(arr[i] & SetBit):
+            x= x^arr[i]
+        else:
+            y = y^arr[i]
+    print("TwoOdd are:",x,"&",y)
+arr = []
+arr_size = int(input("Enter size of the array:"))
+for i in range(0,arr_size):
+    z = int(input("Enter Element:"))
+    arr.append(z)
+TwoOdd(arr,arr_size)
+print(TwoOdd)
